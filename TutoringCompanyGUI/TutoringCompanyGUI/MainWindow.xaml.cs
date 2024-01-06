4d0bac95af1084688fc0ca259d787055bd1599ca
Dawid <dawid.zak.gb22@gmail.com>
2024-01-06 15:59:01 +0100

    Add new files

---
+++ TutoringCompanyGUI/TutoringCompanyGUI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TutoringCompany;
+
+namespace TutoringCompanyGUI {
+    public partial class MainWindow : WindowBase {
+        private ClientList clientList;
+        public MainWindow() {
+            InitializeComponent();
+            base.TitleBar(TitleContent, "Tutoring");
+            clientList = new ClientList();
+        }
+        private void clients_Click(object sender, RoutedEventArgs e) {
+            Clients clientsWindow = new Clients(clientList);
+            clientsWindow.Show();
+        }
+    }
+}
