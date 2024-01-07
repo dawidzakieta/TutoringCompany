9e91eddff8c97224ea29e3e1717c66657b99758e
Kacper <kacper.grabowski@interia.pl>
2024-01-07 20:27:16 +0100

    Zrobienie FrontEndu do AddTutor

--- TutoringCompany/TutoringCompanyGUI/TutoringCompanyGUI/AddTutor.xaml.cs
+++ TutoringCompany/TutoringCompanyGUI/TutoringCompanyGUI/AddTutor.xaml.cs
@@ -18,29 +18,31 @@ using TutoringCompany;
 
 namespace TutoringCompanyGUI
 {
-    public partial class AddTutor : Window
+    public partial class AddTutor : WindowBase
     {
 
         private TutorList tutorList;
+        private ListBox tutorsListBox;
 
-        public AddTutor(TutorList tutorList)
+        public AddTutor(TutorList tutorList, ListBox tutorsListBox)
         {
             InitializeComponent();
+            TitleBar(TitleContent, "Add tutors");
+
             this.tutorList = tutorList;
+            this.tutorsListBox = tutorsListBox;
         }
         private void addTutor_Click(object sender, RoutedEventArgs e)
         {
             decimal rateValue;
-            if (decimal.TryParse(rate.Text, out rateValue))
+            if (decimal.TryParse(tutorRate.Text, out rateValue))
             {
-                Tutor newTutor = new Tutor(name.Text, surname.Text, rateValue, phone.Text, email.Text, (Gender)Enum.Parse(typeof(Gender), ((ComboBoxItem)gender.SelectedItem).Content.ToString()));
+                Tutor newTutor = new Tutor(tutorName.Text, tutorSurname.Text, rateValue, tutorPhone.Text, tutorEmail.Text, (Gender)Enum.Parse(typeof(Gender), ((ComboBoxItem)tutorGender.SelectedItem).Content.ToString()));
                 tutorList.AddTutor(newTutor);
+                tutorsListBox.ItemsSource = new ObservableCollection<Tutor>(tutorList.Tutors);
                 MessageBox.Show("Tutor added correctly.");
             }
-            else
-            {
-                MessageBox.Show("Invalid value for hour rate. Please enter a valid decimal number.");
-            }
+            else MessageBox.Show("Invalid value for hour rate. Please enter a valid decimal number.");
         }
     }
 }
