b4898399df98f44bf85d860116b3e2b86ff82e77
Dawid <dawid.zak.gb22@gmail.com>
2024-01-09 19:35:08 +0100

    a

---
+++ TutoringCompany/TutoringCompany/TutoringCompany/Lesson.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutoringCompany
+{
+    public class Lesson
+    {
+        private Student student;
+        private DateTime date_time;
+
+        public Student Student { get => student; set => student = value; }
+        public DateTime Date_time { get => date_time; set => date_time = value; }
+
+        public Lesson(Student student, DateTime date_time)
+        {
+            this.student = student;
+            this.date_time = date_time;
+        }
+    }
+}
